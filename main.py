42efd163b3016886aecc5a6ceb1650e503b07473
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-01-13 18:07:00 +0530

    feat: added llm and chaining

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
+
 
 load_dotenv()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--task', default="return a list of numbers")
-parser.add_argument('--language', default='python')
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
 args = parser.parse_args()
 
 
 llm = OpenAI()
 
 code_prompt = PromptTemplate(
-    template='Write a very short {language} function that will {task}',
-    input_variables=['language', 'task']
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt
+code_test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test code for the following {language} code: \n{code}",
 )
 
-result = code_chain({
-    "language": args.language,
-    "task": args.task
-})
+code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
 
-print(result)
-print(result['text'])
+code_test_chain = LLMChain(llm=llm, prompt=code_test_prompt, output_key="test")
+
+chain = SequentialChain(
+    chains=[code_chain, code_test_chain],
+    input_variables=["task", "language"],
+    output_variables=["test", "code"],
+)
+
+result = chain({"language": args.language, "task": args.task})
+
+print(">>>>>>>>>>>>> GENERATED CODE:")
+print(result["code"])
+
+print(">>>>>>>>>>>>> GENERATED TEST:")
+print(result["test"])
